94f016fcc0b02629c30e4c90d8e468a20cf100ef
Federico Infantino <infantino.fede@gmail.com>
2022-04-26 13:02:49 +0200

    Template for cart routes

---
+++ src/api/cart.ts
@@ -0,0 +1,12 @@
+import {Request, Response} from 'express';
+
+const express = require('express');
+const router = express.Router();
+
+const file = require('../models/file');
+const Product = require('../models/products');
+const errorList = require('../api/errors');
+
+router.get('/cart', (req, res) =>{
+    
+})
